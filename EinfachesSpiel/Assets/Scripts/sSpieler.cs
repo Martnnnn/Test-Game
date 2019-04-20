9dc06fa1a1fe53f300dc6e662dba8dd43f5559a8
crazyrickpat <patrickhentschel23@googlemail.com>
2019-04-20 21:46:02 +0200

    Spieler Bewegung
    Der Spieler kann sich bewegen

---
+++ EinfachesSpiel/Assets/Scripts/sSpieler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sSpieler : MonoBehaviour
+{
+    int bewegung;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bewegung = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) && bewegung > 0)
+        {
+            transform.Translate(0, 1, 0);
+            bewegung--;
+        }
+        if(Input.GetKeyDown(KeyCode.S) && bewegung > 0)
+        {
+            transform.Translate(0, -1, 0);
+            bewegung--;
+        }
+        if(Input.GetKeyDown(KeyCode.A) && bewegung > 0)
+        {
+            transform.Translate(-1, 0, 0);
+            bewegung--;
+        }
+        if(Input.GetKeyDown(KeyCode.D) && bewegung > 0)
+        {
+            transform.Translate(1, 0, 0);
+            bewegung--;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            bewegung = 5;
+        }
+    }
+}
