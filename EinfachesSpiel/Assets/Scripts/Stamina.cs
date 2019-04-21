9f7a51db19b339bdc96de6f384d081cd4ab738db
Martin <martin.thomas@smail.inf.h-brs.de>
2019-04-22 00:29:00 +0200

    funktionierende Ausdauer Anzeige
    leicht überdimensioniert, aber Darstellungs Teil fertig

--- EinfachesSpiel/Assets/Scripts/Stamina.cs
+++ EinfachesSpiel/Assets/Scripts/Stamina.cs
@@ -3,6 +3,8 @@
 public class Stamina : MonoBehaviour
 {
 
+    public GameObject prefabStaminaBar; //Referenz aus Editor
+    public GameObject[] staminaBars = new GameObject[0];
     SpielerSteuerung globalScript;
 
     // Start is called before the first frame update
@@ -20,6 +22,27 @@ public class Stamina : MonoBehaviour
     {
         //finde Script für den aktuellen Spieler
         StatsCharakter script = globalScript.spieler[globalScript.aktuellerSpieler].GetComponent<StatsCharakter>();
-        Debug.Log(script.bewegung);
+
+        //erzeuge neue Stamina Bars falls mehr benötigt als
+        //erwartet und füge ans array dran
+        if (staminaBars.Length < script.bewegung)
+        {
+            GameObject[] newBars = new GameObject[script.bewegung];
+            for (int i = 0; i < staminaBars.Length; i++)
+            { //erstmal alles alte ins neue
+                newBars[i] = staminaBars[i];
+            }
+            for (int i = staminaBars.Length; i < script.bewegung; i++)
+            { //dann zusätzliche ins neue
+                newBars[i] = Instantiate(prefabStaminaBar, new Vector3(9, -4 + (1.2f * i), 0), Quaternion.identity);
+            }
+            staminaBars = newBars; //pointer aufs neue umlegen
+        }
+
+        //eigentliches Anzeigen
+        for(int i = 0; i < staminaBars.Length; i++)
+        {
+            staminaBars[i].SetActive(script.bewegung > i);
+        }
     }
 }
