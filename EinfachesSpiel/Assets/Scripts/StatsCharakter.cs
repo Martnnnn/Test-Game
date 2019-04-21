52d24a96eeb4658ce0fcb6a91965af21e8113b96
crazyrickpat <patrickhentschel23@googlemail.com>
2019-04-21 22:13:55 +0200

    Script sSpieler umbenannt
    sSpieler heißt nun StatsCharakter in ihm werden zukünftig alle Charakter Stats gespeichert

--- EinfachesSpiel/Assets/Scripts/StatsCharakter.cs
+++ EinfachesSpiel/Assets/Scripts/StatsCharakter.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class sSpieler : MonoBehaviour
 {
+    //Hier kommen alle Charaktter bezogenen Variablen rein (Leben,Schaden usw.)
     public int bewegung;
+    public int leben;
     // Start is called before the first frame update
     void Start()
     {
         bewegung = 5;
+        leben = 100;
     }
 
     // Update is called once per frame
